dc6fa91854190a5abd69d053b9eabe5676eb13b9
maguilla <rafael.maguilla.oliveira@gmail.com>
2016-02-08 00:23:44 -0200

    remove *.txt~ and *.m~ files

--- simulator.m
+++ simulator.m
@@ -1,6 +1,6 @@
 %Variaveis globais, obtidas como resultado de resolucao da P3
 %considerando uma taxa de transmissao de 56Mbits/s
-deta_t = 17.857 %duracao do bit, em ns
+deltaT = 17.857 %duracao do bit, em ns
 T = 66660.181 %duracao da cadeia, em ns
 N = 3733 %numero de bits por cadeia
 %Item (c) - Gerador de sequencias aleatorias de bits
